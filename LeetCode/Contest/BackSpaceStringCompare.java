ab463e19b3d2265f22618659077fba2c9e1c4704
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-04-09 22:18:23 +0530

    Create BackSpaceStringCompare.java
    BackSpace String Compare

---
+++ LeetCode/Contest/BackSpaceStringCompare.java
@@ -0,0 +1,53 @@
+/*
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+*/
+
+import java.util.Stack;
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        
+        int i;
+        
+        Stack<Character> o = new Stack<>();
+        for(i=0;i<=S.length()-1;i++){
+            if(S.charAt(i)!='#')
+                    o.push(S.charAt(i));
+            else if(!o.empty())
+                    o.pop();
+        }
+        
+        Stack<Character> s = new Stack<>();
+        for(i=0;i<=T.length()-1;i++){
+            if(T.charAt(i)!='#')
+                    s.push(T.charAt(i));
+            else if(!s.empty())
+                    s.pop();
+        }
+        
+        return o.equals(s);
+    }
+}
