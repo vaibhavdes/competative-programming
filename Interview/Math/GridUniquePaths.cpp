b23b5e42cf45027b4e7070202ab9de2a16d69620
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-04-24 22:04:52 +0530

    Create GridUniquePaths.cpp
    Grid Unique Paths

---
+++ Interview/Math/GridUniquePaths.cpp
@@ -0,0 +1,36 @@
+/*
+
+A robot is located at the top-left corner of an A x B grid (marked ‘Start’ in the diagram below).
+
+| start  |       |       |       |       |       |        |
+--------------------------------------------------------------
+|        |       |       |       |       |       |        |
+--------------------------------------------------------------
+|        |       |       |       |       |       | finish |
+
+Path Sum: Example 1
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked ‘Finish’ in the diagram below).
+
+How many possible unique paths are there?
+
+Note: A and B will be such that the resulting answer fits in a 32 bit signed integer.
+
+Example :
+
+Input : A = 2, B = 2
+Output : 2
+
+2 possible routes : (0, 0) -> (0, 1) -> (1, 1) 
+              OR  : (0, 0) -> (1, 0) -> (1, 1)
+              
+ */
+
+int Solution::uniquePaths(int A, int B) {
+      long long ans = 1;
+        for(int i = B; i < (A + B -1); i++) {
+            ans *= i;
+            ans /= (i - B + 1);
+        }
+        return (int)ans;
+}
