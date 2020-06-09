0f762f1b4b05e36b32278601de215ba9b8ee1635
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-06-09 19:48:12 +0530

    Create 11. Palindrome String.cpp
    Palindrome String

---
+++ Interview/Strings/11. Palindrome String.cpp
@@ -0,0 +1,28 @@
+/*
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example:
+
+"A man, a plan, a canal: Panama" is a palindrome.
+
+"race a car" is not a palindrome.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+*/
+
+
+int Solution::isPalindrome(string A) {
+    int i = 0;
+    int j = (int) A.size()-1;
+    while (i<j)
+    {
+        while(i<j && !isalnum(A[i])) i++;
+        while(i<j && !isalnum(A[j])) j--;
+        if(tolower(A[i])!=tolower(A[j]))
+            return 0;
+        i++; j--;
+    }
+    return 1;
+}
