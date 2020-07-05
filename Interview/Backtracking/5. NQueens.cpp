5741f61dfffb92cb2f81e6cc4dda991fe50c0fb6
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-07-05 22:08:32 +0530

    Create 5. NQueens.cpp
    NQueens

---
+++ Interview/Backtracking/5. NQueens.cpp
@@ -0,0 +1,28 @@
+/*
+
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+https://i.imgur.com/yaxpgda.png [IMAGE]
+
+N Queens: Example 1
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens’ placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+
+*/
