1d6b9d60fe2ee8633960d06e2f463e4a55626fca
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-07-13 18:43:25 +0530

    Create 1. Remove Half Nodes.cpp
    Remove Half Nodes

---
+++ Interview/Tree Data Structure/1. Remove Half Nodes.cpp
@@ -0,0 +1,105 @@
+/*
+
+Problem Description
+
+Given a binary tree A with N nodes.
+
+You have to remove all the half nodes and return the final binary tree.
+
+NOTE:
+
+Half nodes are nodes which have only one child.
+Leaves should not be touched as they have both children as NULL.
+
+
+Problem Constraints
+1 <= N <= 105
+
+
+
+Input Format
+First and only argument is an pointer to the root of binary tree A.
+
+
+
+Output Format
+Return a pointer to the root of the new binary tree.
+
+
+
+Example Input
+Input 1:
+
+           1
+         /   \
+        2     3
+       / 
+      4
+
+Input 2:
+
+            1
+          /   \
+         2     3
+        / \     \
+       4   5     6
+
+
+Example Output
+Output 1:
+
+           1
+         /    \
+        4      3
+Output 2:
+
+            1
+          /   \
+         2     6
+        / \
+
+       4   5
+
+
+
+Example Explanation
+Explanation 1:
+
+ The only half node present in the tree is 2 so we will remove this node.
+Explanation 2:
+
+ The only half node present in the tree is 3 so we will remove this node.
+
+
+*/
+
+
+
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* A) {
+    if(A==NULL) return A;
+    if(A->left==NULL && A->right == NULL) return A;
+    if(A->left != NULL && A->right != NULL){
+        A->left = solve(A->left);
+        A->right = solve(A->right);
+    }else{
+        if(A->left){
+            A = A->left;
+            return solve(A);
+        }
+        if(A->right){
+            A = A->right;
+            return solve(A);
+        }
+    }
+    return A;
+}
