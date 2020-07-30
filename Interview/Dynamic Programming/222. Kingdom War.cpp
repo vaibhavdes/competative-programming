9293a22b4889c23c4aa9bf59c3a07eb4eadcea53
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-07-30 21:04:17 +0530

    Update 222. Kingdom War.cpp

--- Interview/Dynamic Programming/222. Kingdom War.cpp
+++ Interview/Dynamic Programming/222. Kingdom War.cpp
@@ -39,3 +39,26 @@ Explanation:
 Bomb the sub-matrix from (2,2) to (3,3): 2 + 4 + 5 + 8 = 19
 
 */
+
+
+int Solution::solve(vector<vector<int> > &A) 
+{
+    int n = A.size();
+    int m = A[0].size();
+    
+    vector<int> dp(m, 0);
+    int maxs = 0, tmp = A[0][0];
+    for(int i = n-1; i >= 0; i--)
+    {
+        for(int j = 0; j < m; j++)
+           dp[j] += A[i][j], tmp = max(tmp, A[i][j]);
+          
+        int tmps = 0;
+        for(int j = m-1; j >= 0 && dp[j] > 0; j--)
+            tmps += dp[j];
+        maxs = max(maxs, tmps);
+    }
+    if(maxs == 0)//no positive
+        maxs = tmp;
+    return maxs;
+}
